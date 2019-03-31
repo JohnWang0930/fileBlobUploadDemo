31543f4fd0680baf165e39f3d9e5e631e1d08302
john <wzhen0930@163.com>
2019-03-31 22:52:30 +0800

    [WN] 项目初始化 新建client和server代码

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser  = require('body-parser')
+const fs = require('fs')
+
+const server = express()
+
+server.use(bodyParser.raw({
+    type:'application/octet-stream',
+    limit:'20MB',
+}))
+
+server.post('/',(req,res)=>{
+    console.log(req.body)
+    bufferToFile(req.body,res)
+})
+
+server.listen('3000','127.0.0.1',()=>{
+    console.log('server is running')
+})
+
+function bufferToFile(buffer,res){
+    if (!buffer){
+        return console.log('没有buffer对象传入')
+    }
+    //写入文件
+    fs.writeFile('./1.jpg', buffer, function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send('保存成功');
+        }
+    });
+}
