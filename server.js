8aeb4b3282da8ef5626883dde085022f53d08619
john <wzhen0930@163.com>
2019-04-05 13:18:16 +0800

    [WN] 优化代码

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
 const express = require('express')
-const bodyParser  = require('body-parser')
+const bodyParser = require('body-parser')
 const fs = require('fs')
 
 const server = express()
 
 server.use(bodyParser.raw({
-    type:'application/octet-stream',
-    limit:'20MB',
+    type: 'application/octet-stream',
+    limit: '20MB',
 }))
 
-server.post('/',(req,res)=>{
+server.post('/', (req, res) => {
     console.log(req.body)
-    bufferToFile(req.body,res)
+    bufferToFile(req.body, res)
 })
 
-server.listen('3000','127.0.0.1',()=>{
+server.listen('3000', '127.0.0.1', () => {
     console.log('server is running')
 })
 
-function bufferToFile(buffer,res){
-    if (!buffer){
+function bufferToFile(buffer, res) {
+    if (!buffer) {
         return console.log('没有buffer对象传入')
     }
-    const str = buffer.toString()
-    console.log(str)
     //写入文件
-    fs.writeFile('./1.jpg', Buffer.from(str.split(',')), function (err) {
+    fs.writeFile('./1.jpg', buffer, function (err) {
         if (err) {
             res.send(err);
         } else {
